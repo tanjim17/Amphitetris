39817cb5cac5be1eb762863f06ea1ae3eeda4a79
Jayanta <jayantasadhu4557@gmail.com>
2020-09-19 15:32:31 +0600

    first commit

---
+++ Amphitetris/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Amphitetris project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Amphitetris.settings')
+
+application = get_wsgi_application()
